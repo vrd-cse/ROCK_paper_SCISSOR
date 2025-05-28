c9f82008d54f969755d04cee6c05b65f8758b56c
vrd-cse <btech11103.24@bitmesra.ac.in>
2025-05-28 11:08:20 +0530

    this is updated version of RPS program . it's new update will release shortly . stay tune.

--- Rock_paper_scissor_upadted(2).py
+++ Rock_paper_scissor_upadted(2).py
@@ -1,51 +1,58 @@
 import random
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
+from art import paper , scissors , rock
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+random_dict = {
+    "rock" :rock ,
+    "paper" :paper ,
+    "scissors" : scissors,
+}
+time_win = 0
+total_played_games = 1
+to_continue = True
+while to_continue:
+    
+    #  user choice -->
+    user = input("enter --> Rock , paper , scissors\n").lower()
+    user_choice = random_dict[user]
+    print("You choose this -->")
+    print(user_choice)
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+    # computer choice -->
+    computer_random_guess = random.choice(list(random_dict))
+    print("computer choose this --> ")
+    print(random_dict[computer_random_guess])
 
-computer = [rock , paper , scissors]
-user = int(input("enter a number from , 0 for rock , 1 for paper and 2 for scissors\n"))
-user_output = [rock , paper , scissors]
-computer_random_guess = random.randint(0 , 2)
-print(f"Computer choose {computer_random_guess}")
-print("You choose -->")
-print(user_output[user])
-print("Computer chosse -->")
-print(computer[computer_random_guess])
+    # choosing who won -->
+    if user == computer_random_guess:
+        print("It's a draw!!!")
+    elif user == "rock":
+        if computer_random_guess == "paper" :
+            print("You loose!!!")
+        elif computer_random_guess == "scissors" : 
+            print("You win!!!")
+            time_win +=1 
+    elif user == "paper":
+        if computer_random_guess == "rock":
+            print("You win!!!")
+            time_win +=1 
+        elif computer_random_guess == "scissors" : 
+            print("You loose!!!")
+    elif user == "scissors":
+        if computer_random_guess == "paper" :
+            print("You win!!!")
+            time_win +=1 
+        elif computer_random_guess == "rock":
+            print("You loose!!!")
+    
+    # asking user to continue playing or not
+    ask_uesr_to_continue = input("Type 'Yes '  to continue playing or 'No' to stop \n").lower()
+    
+    if ask_uesr_to_continue == "no" :
+        to_continue == False
+        print(f"You played {total_played_games} and won {time_win}")
+        print("See you soon !!!")
+    else :
+        total_played_games += 1
+    
 
-if computer_random_guess == user :
-    print("It's a draw !!!")
-elif computer_random_guess > user:
-    print("You loose!!!")
-elif computer_random_guess < user:
-    print("You win !!!")
-elif computer_random_guess == 0 and user == 2:
-    print("User loose1 !!!")
-elif computer_random_guess == 2 and user == 0:
-    print("User win !!!")
-else :
-    print("You typed an invalid number . You loose!!!")
-
+    
